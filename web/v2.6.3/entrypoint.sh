d7cda1033f8187eb4b2e29f041fee42e3076c041
Oliver Huang <oliverbhuang@gmail.com>
2021-03-16 17:36:02 -0700

    address comments

--- web/v2.6.3/entrypoint.sh
+++ web/v2.6.3/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 # Run setup
 # The following commands are idempotent, so they can be run before every deploy
@@ -7,9 +7,8 @@ bundle exec rake db:migrate
 bundle exec rake assets:precompile
 printf "$DISCOURSE_ADMIN_EMAIL\n$DISCOURSE_ADMIN_PASSWORD\n$DISCOURSE_ADMIN_PASSWORD\nY" | bundle exec rake admin:create
 
-# Ensure nginx is running
+# Ensure nginx is configured properly
 nginx -t
-service nginx start
 
 # Run Discourse!
 foreman start --procfile=/var/www/discourse/Procfile
